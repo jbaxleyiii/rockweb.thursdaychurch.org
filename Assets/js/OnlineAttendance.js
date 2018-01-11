84d1ad4be23deffeb3845150861ebc969104e187
Jay Greentree <JayGreentree@users.noreply.github.com>
2018-01-11 04:31:01 -0500

    Update OnlineAttendance.js

--- Assets/js/OnlineAttendance.js
+++ Assets/js/OnlineAttendance.js
@@ -15,9 +15,3 @@ var __tracked = 0;
 					   }
 					   });  
 				   }, 0);
-","title":"js","once":false},
-{
-"id":1301,"type":"entrance_url",
-	"trigger_value":"",
-		"action":"wysiwyg_modal",
-			"action_value":"<p><strong>How many people are in the room with you?</strong></p><p class=\"vc-buttons\"></p><p align=\"center\"><button onclick=\"trackVisitorCount(1)\">Just Me></button><button onclick=\"trackVisitorCount(2)\">2</button\><button onclick=\"trackVisitorCount(3)\">3</button><button onclick=\"trackVisitorCount(4)\">4</button><button onclick=\"trackVisitorCount(5)\">5</button><button onclick=\"trackVisitorCount(6)\">6-9</button><button onclick=\"trackVisitorCount(10)\">10+</button></p>","title":"ASK attendance","once":false}],
